b885790b4c297d1911e91e2001f9f8e4c93cec8b
Rostifar <ross_briden@brown.edu>
2019-12-30 22:00:11 -0500

    challenge 2 finished

--- set_1/c_1.py
+++ set_1/c_1.py
@@ -7,6 +7,14 @@ def c_hex_to_dec(c):
     raise Exception("hex character is invalid")
 
 
+def dec_to_c_hex(c):
+    if 0 <= c <= 9:
+        return chr(48 + c)
+    if 10 <= c <= 15:
+        return chr(87 + c)
+    raise Exception("cannot convert decimal to hex")
+
+
 # converts decimal to base64
 def dec_to_c_b64(c):
     if 0 <= c <= 25:
@@ -22,34 +30,49 @@ def dec_to_c_b64(c):
     raise Exception("cannot convert decimal to base64")
 
 
-def dec_str_to_hex(s):
-    return [dec_str_to_hex(c) for c in s]
+def c_b64_to_dec(c):
+    if ord('A') <= c <= ord('Z'):
+        return c - 65
+    if ord('a') <= c <= ord('z'):
+        return c - 71
+    if ord('0') <= c <= ord('9'):
+        return c - 4
+    if ord(c) == 43:
+        return 62
+    if ord(c) == 47:
+        return 63
+    raise Exception("cannot convert decimal to base64")
+
 
 
 def hex_str_to_dec(s):
     return [c_hex_to_dec(c) for c in s]
 
 
+def b64_str_to_dec(s):
+    return [c_b64_to_dec(c) for c in s]
+
+
 # converts hex to base64
 def hex_to_b64(s):
+    # convert to raw string
     b = list(s.encode())
     b_len = len(b)
 
     if b_len == 0:
         return ''
 
+    # add zero padding if hex string has odd length
     hex_padded = False
     if b_len % 2 != 0:
         b.insert(0, 0)
         hex_padded = True
-    i = 0
 
+    i, bits_proc, idx = 0, 0, 0
     f_mask = 15
     r_mask = 12
 
     s = []
-    bits_proc = 0
-    idx = 0
     while idx < b_len:
         first = 0 if i == 0 and hex_padded else c_hex_to_dec(b[idx]) & f_mask
         rest = c_hex_to_dec(b[idx + 1]) & r_mask if idx + 1 < b_len else -1
@@ -65,8 +88,8 @@ def hex_to_b64(s):
         s.append(tmp)
 
         bits_proc += 6
-
         i += 1
+
         if i % 2 == 0:
             idx += 2
             f_mask = 15
@@ -84,3 +107,16 @@ def hex_to_b64(s):
         s += '='
         r -= 6
     return s
+
+
+# inputs string in hex
+def xor(s1, s2):
+    b1 = hex_str_to_dec(s1.encode())
+    b2 = hex_str_to_dec(s2.encode())
+
+    b3 = []
+    for i in range(len(b1)):
+        b3.append((b1[i] ^ b2[i]) & 15)
+    print(b3)
+    return ''.join([dec_to_c_hex(c) for c in b3])
+
